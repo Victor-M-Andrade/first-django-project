214ca6c19dac769c4b9efe50fbaf3789e7ca816c
Victor-M-Andrade <victorma2000@live.com>
2022-12-21 11:54:59 -0300

    add dinamic posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #com filtro
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #sem filtro
+    posts = Post.objects.all().order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
