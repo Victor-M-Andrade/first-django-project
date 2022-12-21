ca627294befa15b11fa9d419503f1442d2b30717
Victor-M-Andrade <victorma2000@live.com>
2022-12-21 14:15:26 -0300

    remove post_detail and 'a' tag to 'h2' tags in post_list

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,3 @@ def post_list(request):
     #sem filtro
     posts = Post.objects.all().order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
-
-def post_detail(request, pk):
-    post = get_list_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post':post})
